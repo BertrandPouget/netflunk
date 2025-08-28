5a4c06e125c8772c212e37f7f146fe03570c7912
BertrandPouget <markhillsxd@gmail.com>
2025-08-28 18:34:11 +0200

    v1 working

--- app/data/items.ts
+++ app/data/items.ts
@@ -23,8 +23,8 @@ export const ITEMS: Item[] = [
       "Un gruppo di studenti scopre Monaco per la prima volta, tra imprevisti esilaranti e legami che segneranno l’inizio di un viaggio leggendario.",
     poster: "/flunkgasse_cover.png",
     background: "/backgrounds/flunkgasse_bg.png",
-    src: "https://drive.google.com/file/d/1g5G-222uqzTFFxKMvsvb79AxtC5EIKE8/preview",
-    trailerSrc: "https://drive.google.com/file/d/1ZBI7vkgEKbJTPDEelwiEF6AXOhh8kBDe/preview",
+    src: "https://drive.google.com/file/d/1cmj7oeBG4D8V9-9dwbCkJFDACPbXfrwk/preview",
+    trailerSrc: "https://drive.google.com/file/d/1R2JPARhrdk8itj1Zfw2coqehH-kT0baL/preview",
   },
   {
     slug: "kraillinger",
@@ -36,8 +36,8 @@ export const ITEMS: Item[] = [
       "Di ritorno a Monaco, il gruppo si trova di fronte a una misteriosa porta che li trascina in un’avventura sospesa tra amicizia, conflitti e segreti nascosti.",
     poster: "/kraillinger_cover.png",
     background: "/backgrounds/kraillinger_bg.png",
-    src: "https://drive.google.com/file/d/12KiT79uKGjxI2ZLWT_tEjMAyEChCTmmk/preview",
-    trailerSrc: "https://drive.google.com/file/d/1IamEck3YqpUIy9vdRoxhkKIO1SWkDHth/preview",
+    src: "https://drive.google.com/file/d/13nOfx6H8ErDAhjgY8xwDaoWhId9-uu6v/preview",
+    trailerSrc: "https://drive.google.com/file/d/1-_oDImCtzYn-qk_K_1pOQl-DXtcfocZa/preview",
   },
   {
     slug: "holzkirchen",
@@ -49,8 +49,8 @@ export const ITEMS: Item[] = [
       "Tra canzoni improvvisate, incontri assurdi e fiumi di alcol, i ragazzi continuano il loro viaggio sempre più lontano da Monaco, senza sapere cosa li aspetta.",
     poster: "/holzkirchen_cover.png",
     background: "/backgrounds/holzkirchen_bg.png",
-    src: "https://drive.google.com/file/d/122LDc1fw3BrRwrzf1N2snzSPRBZ84O5k/preview",
-    trailerSrc: "https://drive.google.com/file/d/1x_ll7oJL-FUSXTEZkPvCXBWohAqI4byl/preview",
+    src: "https://drive.google.com/file/d/1_-t3tcmfI5OGLtit6awZy5I7cHQO7aX8/preview",
+    trailerSrc: "https://drive.google.com/file/d/1fGmXYWj_0XmYpvPZ16v1uaFjPQCiluSa/preview",
   },
   {
     slug: "cyberpub",
@@ -62,8 +62,8 @@ export const ITEMS: Item[] = [
       "Oltre la Manica, un pub sospeso tra reale e digitale diventa il fulcro di complotti e alleanze inattese. Tra neon e ombre, il confine con il virtuale si fa sempre più labile.",
     poster: "/cyberpub_cover.png",
     background: "/backgrounds/cyberpub_bg.png",
-    src: "https://drive.google.com/file/d/1VWMY9xLoYAh8mjulcKk-KRGHx4REao3u/preview",
-    trailerSrc: "https://drive.google.com/file/d/1CWrANVkugQv63P88AB8pqsbnIMCU1p8b/preview",
+    src: "https://drive.google.com/file/d/1EUVyAQthFou385UYSdkQ1Yu-B2psGhOX/preview",
+    trailerSrc: "https://drive.google.com/file/d/16vEzwzSz7_-ENPAFXoXRtiAMrBKVk9Qy/preview",
   },
   {
     slug: "blitzkrieg",
@@ -75,7 +75,7 @@ export const ITEMS: Item[] = [
       "Un’indagine tra due epoche diverse porta alla ricerca di un assassino che sfugge al tempo stesso. Misteri, indizi e verità distorte si intrecciano in un finale inaspettato.",
     poster: "/blitzkrieg_cover.png",
     background: "/backgrounds/blitzkrieg_bg.png",
-    src: "https://drive.google.com/file/d/1NP1-6dcq6l0r1AKalpABurBTwuLBhulI/preview",
-    trailerSrc: "https://drive.google.com/file/d/1ig_McWmNJiNZMY_HifydBKChhmCyTqcB/preview",
+    src: "https://drive.google.com/file/d/12XeHmjEU1JJ31n8dDruc_ThkchWie33S/preview",
+    trailerSrc: "https://drive.google.com/file/d/1DsL8xAbUnVQQgusl2dBj1a7-CoksnXAP/preview",
   },
 ];
